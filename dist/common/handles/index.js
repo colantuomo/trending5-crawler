c80c4119449c5a145759dac518bb629b7b2f36c5
Paulo Martins <pcolantuomo@gmail.com>
2021-02-13 15:37:37 -0300

    delete dist

--- dist/common/handles/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleError = exports.handleSuccess = void 0;
-const db_1 = require("../../db");
-const handleSuccess = (crawler, topic, items) => db_1.db.save({ crawler, topic, items });
-exports.handleSuccess = handleSuccess;
-const handleError = (error, crawler) => console.error({ crawler, date: new Date(), error });
-exports.handleError = handleError;
-//# sourceMappingURL=index.js.map
--- dist/src/common/enums/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Topics = void 0;
-var Topics;
-(function (Topics) {
-    Topics["Trending"] = "trending";
-    Topics["Economy"] = "economy";
-})(Topics = exports.Topics || (exports.Topics = {}));
-//# sourceMappingURL=index.js.map
